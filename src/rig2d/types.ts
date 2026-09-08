313a5233dcc5748f080b3254a6793a512e986277
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-08 13:03:16 +0300

    Add Rig2D core types

---
+++ src/rig2d/types.ts
@@ -0,0 +1,83 @@
+export interface Vec2 {
+  x: number;
+  y: number;
+}
+
+export interface Transform2D {
+  x: number;
+  y: number;
+  rotation: number;
+  scaleX: number;
+  scaleY: number;
+}
+
+export type ParentAnchor = 'origin' | 'end';
+
+export interface BoneDefinition {
+  name: string;
+  parent?: string;
+  anchor?: ParentAnchor;
+  x?: number;
+  y?: number;
+  rotation?: number;
+  length?: number;
+  minRotation?: number;
+  maxRotation?: number;
+}
+
+export interface BonePose {
+  x?: number;
+  y?: number;
+  rotation?: number;
+  scaleX?: number;
+  scaleY?: number;
+}
+
+export type RigPose = Record<string, BonePose>;
+
+export interface BoneSnapshot {
+  name: string;
+  parent?: string;
+  start: Vec2;
+  end: Vec2;
+  worldRotation: number;
+  local: Transform2D;
+  length: number;
+}
+
+export interface TwoBoneIKOptions {
+  upper: string;
+  lower: string;
+  target: Vec2;
+  bend?: -1 | 1;
+  pole?: Vec2;
+  weight?: number;
+  stretch?: number;
+}
+
+export interface AimConstraintOptions {
+  bone: string;
+  target: Vec2;
+  weight?: number;
+  offset?: number;
+}
+
+export interface RigAttachmentDefinition {
+  name: string;
+  bone: string;
+  x?: number;
+  y?: number;
+  rotation?: number;
+}
+
+export interface RigAttachmentSnapshot {
+  name: string;
+  bone: string;
+  position: Vec2;
+  rotation: number;
+}
+
+export interface RigDebugSnapshot {
+  bones: BoneSnapshot[];
+  attachments: RigAttachmentSnapshot[];
+}
