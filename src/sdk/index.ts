dcc3fb8c6aeaa6408a2e3ecdec543644b1bb9523
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-08 17:15:43 +0300

    Export PixiLive Nova SDK surface

---
+++ src/sdk/index.ts
@@ -0,0 +1,8 @@
+export { NovaLiveController } from './NovaLiveController';
+export type { NovaLiveControllerOptions, NovaLiveControllerEvents } from './NovaLiveController';
+export { NovaFlightController } from './NovaFlightController';
+export type { FlyToOptions, Point2D } from './NovaFlightController';
+export { PixiLiveNovaElement, definePixiLiveNovaElement } from './PixiLiveNovaElement';
+export type { PerformanceCue, CharacterAffect, CharacterGesture, CharacterMode } from '../character/performance';
+
+export const PIXILIVE_SDK_VERSION = '0.1.0-alpha.1';
