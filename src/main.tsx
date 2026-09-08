06a81155f56fc520b2bbe8a318044825057a59e7
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-08 11:24:46 +0300

    Load session log styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './performance.css';
+import './session-log.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
