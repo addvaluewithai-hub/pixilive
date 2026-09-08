5245c1b7af316815ae4f8c8105dd88c86044b918
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-08 14:16:43 +0300

    Export continuous Rig2D Pixi skin helpers

--- src/rig2d/index.ts
+++ src/rig2d/index.ts
@@ -1,5 +1,7 @@
 export { Rig2D } from './Rig2D';
 export { PixiRigBinding } from './PixiRigBinding';
+export { drawChainSkin, drawTaperedTube } from './PixiChainSkin';
+export type { ChainSkinStyle, TubeStyle } from './PixiChainSkin';
 export { SpringScalar, SpringVec2 } from './spring';
 export * from './math';
 export type * from './types';
